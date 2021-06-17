cca055c5d89ba992a7cc74b7c5a8d94cd935b810
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-06-17 11:39:34 +0100

    Disable mouse capture (not used)

--- roomba-cli/src/tui.rs
+++ roomba-cli/src/tui.rs
@@ -6,7 +6,7 @@ use roomba::{api, Client};
 use std::thread;
 use std::{error::Error, io};
 use termion::input::TermRead;
-use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+use termion::{event::Key, raw::IntoRawMode, screen::AlternateScreen};
 use tui::widgets::ListState;
 use tui::{
     backend::TermionBackend,
@@ -118,7 +118,6 @@ impl<'a> App<'a> {
     pub async fn main_loop(mut self) -> Result<Option<Map>, Box<dyn Error>> {
         // Terminal initialization
         let stdout = io::stdout().into_raw_mode()?;
-        let stdout = MouseTerminal::from(stdout);
         let stdout = AlternateScreen::from(stdout);
         let backend = TermionBackend::new(stdout);
         let mut terminal = Terminal::new(backend)?;
